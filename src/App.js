41224a0d75eaee6bd638e86858754f712ddb9ef1
Baptiste-Caral <baptiste.caral@gmail.com>
2020-08-23 23:48:58 +0200

    scrollbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 const options = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
 
 function App() {
-
   return (
     <div className="App"> 
       <Select options={options} />
